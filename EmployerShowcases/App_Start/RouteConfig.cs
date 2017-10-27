c2ac0b5df6210543b979255b4f4c4a39c1d20346
Joshua Anker <josh1234@gmail.com>
2017-10-27 15:34:39 -0400

    Changed default controller in RouteConfig.cs

--- EmployerShowcases/App_Start/RouteConfig.cs
+++ EmployerShowcases/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EmployerShowcases
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "EmployerShowcase", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
